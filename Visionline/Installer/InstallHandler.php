cd5fcdf5dac24f937c6c024810957602e8111a09
sebastianblum <sb@sblum.de>
2014-03-31 15:27:08 +0200

    test

--- Visionline/Installer/InstallHandler.php
+++ Visionline/Installer/InstallHandler.php
@@ -10,12 +10,22 @@ class InstallHandler
 	{
 		$composer = $event->getComposer();
 		$extra = $event->getComposer()->getPackage()->getExtra();
-		
-		$output = '';		
-		$output .= print_r($composer, true) . "\n\n\n\n###############################\n\n\n\n";
-		$output .= print_r($extra) . "\n\n\n\n###############################\n\n\n\n";
-		$output .= print_r($event) . "\n\n\n\n###############################\n\n\n\n";
-		
-		file_put_contents('output.log', $output);
+
+		if (!array_key_exists('visionline-webclient-client', $extra)
+            || !array_key_exists('phar', $extra['visionline-webclient-client'])
+        ) {
+            throw new \RuntimeException(
+                sprintf(
+                    "Please specify the path to the phar in composer.json\n\n\s",
+                    json_encode(array('visionline-webclient-client' => array('phar' => '...')))
+                )
+            );
+        }
+
+        $pharPath = $extra['visionline-webclient-client']['phar'];
+        $event->getIO()->write(sprintf('Downloading Visionline phar from "%s"', $pharPath));
+
+        print_r($composer->getConfig());
+		print_r($event->getOperation()->getPackage(););
 	}
 }
