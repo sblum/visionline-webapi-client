3ed972a7e7e34c086e24462f6843e0a31e33a95f
sebastianblum <sb@sblum.de>
2014-03-31 17:38:48 +0200

    Downloading Phar

--- Visionline/Installer/InstallHandler.php
+++ Visionline/Installer/InstallHandler.php
@@ -16,16 +16,29 @@ class InstallHandler
         ) {
             throw new \RuntimeException(
                 sprintf(
-                    "Please specify the path to the phar in composer.json\n\n\s",
+                    "Please specify the path to the phar in composer.json\n\n%s",
                     json_encode(array('extra' => array('visionline-webclient-client' => array('phar' => '...'))))
                 )
             );
         }
 
-        $pharPath = $extra['visionline-webclient-client']['phar'];
-        $event->getIO()->write(sprintf('Downloading Visionline phar from "%s"', $pharPath));
+        $pharUri= $extra['visionline-webclient-client']['phar'];
+        $event->getIO()->write(sprintf('Downloading Visionline phar from "%s"', $pharUri));
 
-        $operation = $event->getOperation();
-        print_r($operation);
+		$pharPath = __DIR__ . '/../Download/visionline.phar';
+		$event->getIO()->write(sprintf('Saving phar at "%s"', $pharPath));
+		
+		$ch=curl_init();   
+		curl_setopt($ch, CURLOPT_URL, $pharUri);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+		curl_setopt($ch, CURLOPT_HEADER,0); 
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		file_put_contents($pharPath, $response);
+		
+		
     }
 }
