2795bd62422d345b32c176819e00df5569f39dd0
Sebastian Blum <github@sblum.de>
2017-09-19 10:31:51 +0200

    Erweiterung Anfrage und Rückgabe der ID bei der Erstellung einer Anfrage

---
+++ Visionline/Crm/WebApi/StoredEnquiry.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Defines the class StoredEnquiry
+ * @package pagepackage
+ */
+
+namespace Visionline\Crm\WebApi;
+
+/**
+ * Describes an enquiry stored in CRM-VISIONLINE
+ */
+class StoredEnquiry extends Enquiry
+{
+  /**
+   * Die ID der Anfrage
+   * @var int
+   */
+  public $id;
+
+  /**
+   * Der Status der Anfrage
+   * @var string
+   */
+  public $status;
+
+  /**
+   * ID des zugeordneten Kontakts.
+   * @var int
+   */
+  public $kontaktId;
+
+  /**
+   * IDs der zugeordneten Objekte.
+   * @var array of int
+   */
+  public $objektIds;
+
+  /**
+   * IDs der zugeordneten Projekte.
+   * @var array of int
+   */
+  public $projektIds;
+
+  /**
+   * IDs der zugeordneten Dokumente.
+   * @var array of int
+   */
+  public $dokumentIds;
+
+  /**
+   * IDs der zugeordneten Betreuer.
+   * @var array of int
+   */
+  public $betreuerIds;
+
+  /**
+   * IDs der Kontakte der zugeordneten Betreuer.
+   * @var array of int
+   */
+  public $betreuerKontaktIds;
+}
