bb84d05064d6ffcdcf769fdb9f506854faa7f42a
Sebastian Blum <github@sblum.de>
2018-02-10 17:44:16 +0100

    first unit tests

---
+++ Visionline/Tests/WebApiTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Visionline\Tests\Crm\WebApi;
+
+use Visionline\Crm\WebApi\Connection;
+use PHPUnit\Framework\TestCase;
+use Visionline\Crm\WebApi\WebApi;
+
+class WebApiTest extends TestCase
+{
+    const HOST = 'app.visionline.at';
+    const PORT = 5000;
+    const USERNAME = 'testUser';
+    const PASSWORD = 'testPassword';
+    const ENDPOINT = 'https://app2.visionline.at:8443/WebApi/WebApi.asmx?WSDL';
+
+    public function testConnection()
+    {
+        $connection = new Connection(
+            self::HOST,
+            self::PORT,
+            self::USERNAME,
+            self::PASSWORD
+        );
+
+        $this->assertAttributeSame(self::HOST, 'host', $connection);
+        $this->assertAttributeSame(self::PORT, 'port', $connection);
+        $this->assertAttributeSame(self::USERNAME, 'username', $connection);
+        $this->assertAttributeSame(self::PASSWORD, 'password', $connection);
+    }
+
+    public function testWebApi()
+    {
+        $connection = new Connection(
+            self::HOST,
+            self::PORT,
+            self::USERNAME,
+            self::PASSWORD
+        );
+
+        $webApi = new WebApi(
+            self::ENDPOINT,
+            $connection,
+            [
+                'debug' => true,
+            ]
+        );
+
+        $this->assertAttributeSame($connection, 'connection', $webApi);
+        $this->assertAttributeSame(self::ENDPOINT, 'endpoint', $webApi);
+    }
+}
